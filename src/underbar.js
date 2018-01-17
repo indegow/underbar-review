9ad75dbd15a8f5702077af7b0b7fc0705485f090
indegow <indegow@aol.com>
2018-01-16 20:33:19 -0800

    Exicuted Some & some tests

--- src/underbar.js
+++ src/underbar.js
@@ -231,6 +231,12 @@
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if (iterator === undefined) {
+      iterator = _.identity; 
+    }
+    return !_.every(collection, function(a, b) {
+      return !iterator(a, b);
+    });
   };
 
 
